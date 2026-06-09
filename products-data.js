2b3b4839268079cb46b2baeee01488abaf3e1b27
tubayvz <tuba.yavuz@stu.fbu.edu.tr>
2026-06-10 02:20:19 +0300

    Add files via upload

---
+++ products-data.js
@@ -0,0 +1,34 @@
+// turkishdelight-products.js
+const allProductsData = [
+    {
+        id: 1,
+        name: "Olive Green Trench Coat",
+        price: 2450,
+        imgSrc: "5928ef32007d796dd3725ca20d84423c.jpg", // Klasördeki görsel adın neyse o olmalı
+        category: "Coat"
+    },
+    {
+        id: 2,
+        name: "Floral Embroidered Set",
+        price: 1980,
+        imgSrc: "c929d405cee969889eb21a95ec895221.jpg",
+        category: "Set"
+    },
+    {
+        id: 3,
+        name: "Sage Green Overshirt",
+        price: 3400,
+        imgSrc: "8deddffceff6cee0da4c3d50443423c7.jpg",
+        category: "Shirt"
+    },
+    {
+        id: 4,
+        name: "Classic Denim Jacket",
+        price: 1650,
+        imgSrc: "a3345b371610fa3004c17055baee3f8d.jpg",
+        category: "Jacket"
+    }
+];
+
+// Diğer sayfalar bu veriyi kullanabilsin diye dışa aktarıyoruz
+export default allProductsData;
